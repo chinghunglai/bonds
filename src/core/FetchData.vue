cdf4e92dff22df575163e9010ac06e4649964491
chin <>
2024-03-27 12:30:43 +0800

    以 json 格式#1

--- src/core/FetchData.vue
+++ src/core/FetchData.vue
@@ -15,48 +15,75 @@ export default {
 	beforeDestroy() {},
 	components: {},
 	methods: {
-		fetchDataSource() {
-			fetch(`./data/${vuex.dataSource}.txt`).then(res=>res.text()).then(res=>{
-				res.split('[NEW-LINE]').forEach(line=>{
-					let cols = line.split('\t');
-					if (cols[1] && cols[2]) {
-						let isin = cols[1].trim();
-						let bond = {isin};
-						this.setValue({bond, key: 'comp', val: cols[2]});
-						this.setValue({bond, key: 'name', val: cols[3]});
-						this.setValue({bond, key: 'currency', val: cols[4]});
-						this.setValue({bond, key: 'sellPrice', val: cols[5]});
-						this.setValue({bond, key: 'buyPrice', val: cols[7]});
-						this.setValue({bond, key: 'yield', val: cols[16]});
-						this.setValue({bond, key: 'redemptionYear', val: cols[17]});
-						this.setValue({bond, key: 'endYear', val: cols[18]});
-						this.setValue({bond, key: 'rating1', val: cols[19]});
-						this.setValue({bond, key: 'rating2', val: cols[21]});
-						this.setValue({bond, key: 'rating3', val: cols[23]});
-						this.setValue({bond, key: 'couponRate', val: cols[25]});
-						this.setValue({bond, key: 'couponCount', val: cols[26]});
-						this.setValue({bond, key: 'nextPaymentDate', val: cols[27]});
-						this.setValue({bond, key: 'riskLevel', val: cols[54]});
-						this.setValue({bond, key: 'repaymentOrder', val: cols[56]});
-						if (bond.sellPrice < 0 || bond.buyPrice < 0)
-							return;
-						if (bond.repaymentOrder.indexOf('主順位非優先受償無擔保')!==-1)
-							bond.repaymentOrder = '主順位';
-						// 年折溢價
-						bond.plInOneYear = vuex.fn.$safeFloat((bond.buyPrice - 100) / bond.endYear * 2000);
-						// 年收益
-						bond.incomeInOneYear = vuex.fn.$safeFloat(Number(bond.couponRate)*2000 - bond.buyPrice*2000*vuex.lendingRates/100 - bond.plInOneYear);
-						// 年收益負值不往下算
-						if (bond.incomeInOneYear < 0) {bond.incomeInOneYear = ''; return;}
-						// 年收益率
-						bond.incomeInOneYearRate =  vuex.fn.$safeFloat(bond.incomeInOneYear / (bond.buyPrice * 2000) * 100);
-						// 最大收益
-						bond.maxIncome =  vuex.fn.$safeFloat(bond.incomeInOneYear * bond.endYear);
-						// 最大收益%
-						bond.maxIncomeRate =  vuex.fn.$safeFloat(bond.incomeInOneYearRate * bond.endYear);
-						vuex.bondMap[isin] = bond;
+		findHead(totalObj) {
+			for (let key1 in totalObj) {
+				let bigObj = totalObj[key1];
+				for (let i=0; i<bigObj.length; i++) {
+					let bondString = JSON.stringify(bigObj[i]);
+					if (bondString.indexOf('發行人') !== -1 && bondString.indexOf('證券名稱') !== -1) {
+						return bigObj[i];
 					}
-				});
+				};
+			}
+		},
+		analysisColumn(totalObj) {
+			let headObj = this.findHead(totalObj);
+			let reverseHeadObj = {};
+			for (let key in headObj) {
+				reverseHeadObj[headObj[key]] = key;
+			}
+			for (let key in vuex.csMap) {
+				let csObj = vuex.csMap[key];
+				let headKey = csObj.head;
+				if (headKey) {
+					csObj.column = reverseHeadObj[headKey];
+				}
+			}
+		},
+		// 更新填入 bond 資料
+		updateBond(bondObj) {
+			if (!bondObj || !bondObj.Column9)
+				return;
+			let csMap = vuex.csMap;
+			let bond = {};
+			// 將所有資料欄位填入 bond 物件
+			for (let key in csMap) {
+				if (csMap[key].column) {
+					let val = bondObj[csMap[key].column];
+					this.setValue({bond, key, val});
+				}
+			}
+			// 順位
+			bond.repaymentOrder = '' + bond.repaymentOrder;
+			if (bond.repaymentOrder.indexOf('主順位非優先受償無擔保')!==-1)
+				bond.repaymentOrder = '主順位';
+			// 無正常買賣價不往下算
+			if (bond.sellPrice < 0 || bond.buyPrice < 0)
+				return;
+			// 年折溢價
+			bond.plInOneYear = vuex.fn.$safeFloat((bond.buyPrice - 100) / bond.endYear * 2000);
+			// 年收益
+			bond.incomeInOneYear = vuex.fn.$safeFloat(Number(bond.couponRate)*2000 - bond.buyPrice*2000*vuex.lendingRates/100 - bond.plInOneYear);
+			// 年收益負值不往下算
+			if (bond.incomeInOneYear < 0) {bond.incomeInOneYear = ''; return;}
+			// 年收益率
+			bond.incomeInOneYearRate =  vuex.fn.$safeFloat(bond.incomeInOneYear / (bond.buyPrice * 2000) * 100);
+			// 最大收益
+			bond.maxIncome =  vuex.fn.$safeFloat(bond.incomeInOneYear * bond.endYear);
+			// 最大收益%
+			bond.maxIncomeRate =  vuex.fn.$safeFloat(bond.incomeInOneYearRate * bond.endYear);
+			let isin = bondObj[csMap.isin.column];
+			vuex.bondMap[isin] = bond;
+		},
+		fetchDataSource() {
+			fetch(`./data/金交債0325原.json`).then(res=>res.json()).then(totalObj=>{
+				this.analysisColumn(totalObj);
+				for (let key1 in totalObj) {
+					let bigObj = totalObj[key1];
+					bigObj.forEach(bondObj=>{
+						this.updateBond(bondObj);
+					});
+				};
 				this.fetchDataSource2();
 			});
 		},
@@ -90,7 +117,7 @@ export default {
 			bond[key] = val;
 			let cso = vuex.csMap[key];
 			if (val && cso.isNumber)
-				bond[key] = vuex.fn.parseNumberWithParentheses(val.trim());
+				bond[key] = vuex.fn.parseNumberWithParentheses(val);
 		},
 	},
 	watch: {},
